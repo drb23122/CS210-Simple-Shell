6cf898106da141eb62a1f86445f61d2aeb1b7850
finlaysr <60317020+finlaysr@users.noreply.github.com>
2026-01-29 12:35:22 +0000

    Small fix for flashing cursor not displaying properly

--- include/input.h
+++ include/input.h
@@ -3,10 +3,10 @@
 #define BLINK "\x1b[5m"
 
 #define ANSI_RESET "\x1b[0m"
-#define ANSI_RED "\x1b[31m"
-#define ANSI_GREEN "\x1b[32m"
-#define ANSI_BLUE "\x1b[34m"
-#define ANSI_MAGENTA "\x1b[35m"
+#define ANSI_RED ANSI_RESET "\x1b[31m"
+#define ANSI_GREEN ANSI_RESET "\x1b[32m"
+#define ANSI_BLUE ANSI_RESET "\x1b[34m"
+#define ANSI_MAGENTA ANSI_RESET "\x1b[35m"
 
 #define ANSI_RED_BG ANSI_RESET "\x1b[41m"
 #define ANSI_GREEN_BG ANSI_RESET "\x1b[42m"
