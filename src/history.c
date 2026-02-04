ba9ebf29925587a523ed52c1b0019e16ac727e21
finlaysr <60317020+finlaysr@users.noreply.github.com>
2026-02-04 18:14:33 +0000

    Attempt at solving history overflow by moving all elemets forward by one, doesn't work

--- src/history.c
+++ src/history.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-char *history[INPUT_LEN][HIST_LEN];
+char *history[HIST_LEN][INPUT_LEN];
 int total = 0;
 
-// returns 1 if history error, else zero
+// returns 1 if error in parsing history command, else zero
 int check_hist(char *tokens[INPUT_LEN]) {
   if (!tokens[0]) {
     return 1;
@@ -19,20 +20,27 @@ int check_hist(char *tokens[INPUT_LEN]) {
       return 1;
     }
 
-    char num_s[4];
-    strncpy(num_s, tokens[0] + 1, 3);
-    int num = atoi(num_s);
-    if (num < -HIST_LEN || num > HIST_LEN) {
-      printf("History goes from position -%d to %d!\n", HIST_LEN, HIST_LEN);
-      return 1;
-    }
+    // Determint history number entered
     int pos;
-    if (num > 0) {
-      pos = num - 1;
+    if (tokens[0][1] == '!') { // If !! entered
+      pos = total;
     } else {
-      pos = total + num;
+      char num_s[4];
+      strncpy(num_s, tokens[0] + 1, 3);
+      int num = atoi(num_s);
+      if (num < -total || num > total || num == 0) {
+        printf("History goes from position -%d to %d!\n", -total, total);
+        return 1;
+      }
+      if (num > 0) {
+        pos = num - 1;
+      } else {
+        pos = total + num;
+      }
     }
+    printf("Pos: %d\n", pos);
 
+    // Subsitute history element into tokens input
     for (int i = 0; history[pos][i]; i++) {
       tokens[i] = history[pos][i];
     }
@@ -45,16 +53,37 @@ int check_hist(char *tokens[INPUT_LEN]) {
 }
 
 int add_hist(char *input[INPUT_LEN]) {
-  for (int i = 0; input[i]; i++) {
-    history[total][i] = malloc((strlen(input[i]) + 1) * sizeof(char));
-    strcpy(history[total][i], input[i]);
+
+  if (total < HIST_LEN) {
+    for (int i = 0; input[i]; i++) {
+      history[total][i] = malloc((strlen(input[i]) + 1) * sizeof(char));
+      strcpy(history[total][i], input[i]);
+    }
+    total++;
+  } else {
+
+    // Move each element forward one
+    for (int i = 0; i < HIST_LEN - 1; i++) {
+      for (int j = 0; j < INPUT_LEN; j++) {
+        history[i][j] = history[i + 1][j];
+      }
+    }
+
+    // Clear last element
+    for (int i = 0; history[HIST_LEN - 1][i]; i++) {
+      free(history[HIST_LEN - 1][i]);
+      history[HIST_LEN - 1][i] = NULL;
+    }
+
+    for (int i = 0; input[i]; i++) {
+      history[HIST_LEN - 1][i] = malloc((strlen(input[i]) + 1) * sizeof(char));
+      strcpy(history[HIST_LEN - 1][i], input[i]);
+    }
   }
-  total++;
   return 0;
 }
 
 void print_hist() {
-  printf("");
   for (int i = 0; i < HIST_LEN; i++) {
     printf("%2d: ", i + 1);
     for (int j = 0; history[i][j]; j++) {
--- src/main.c
+++ src/main.c
@@ -49,6 +49,7 @@ int main(void) {
         run(tokens);
       }
     }
+    print_hist();
 
     clear(tokens);
   }
@@ -58,5 +59,5 @@ int main(void) {
   setpath(saved_path);
   free(saved_path[1]);
   printf("Restored path: %s\n", getenv("PATH"));
-  free_hist();
+  // free_hist();
 }
